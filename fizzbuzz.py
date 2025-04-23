f5e70f54b4a3d16349b184f8009b908e93c60625
runefor <fkjy132.gmail.com>
2025-04-23 10:27:07 +0900

    feat: Add print statement for multiples of 5

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,6 +1,8 @@
-for i in range(1, 17):
+for i in range(1, 21):
     if i % 3 == 0:
         print(f"fizz: {i}")
+    elif i % 5 == 0:
+        print(f"buzz: {i}")
     else:
         print(f"not fizee: {i}")
 print("It works!")
