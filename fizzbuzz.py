507c7e22d4c6d9b119e73b0136e52ae3c3b8a7d6
runefor <fkjy132.gmail.com>
2025-04-23 11:13:12 +0900

    feat: Updated condition to print when a number is divisible by 3 or 5

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,9 +1,5 @@
-for i in range(1, 18):
-    if i % 15 == 0:
-        print(f"fizzbuzz: {i}")
-    elif i % 3 == 0:
-        print(f"fizz: {i}")
-    elif i % 5 == 0:
-        print(f"buzz: {i}")
+for i in range(1, 20):
+    if i % 3 == 0 or i % 5 == 0:
+        print("Fizz" * (i % 3 == 0) + "Buzz" * (i % 5 == 0))
     else:
         print(f"not fizee: {i}")
