10d731725bb8900a50a84275e2a12c601f9d848a
runefor <fkjy132.gmail.com>
2025-04-23 12:15:03 +0900

    refactor: fizzbuzz logic

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,6 +1,6 @@
 # if문 1개로 fizzbuzz 구현하기
 for i in range(1, 20):
-    if i % 15 == 0:
-        print("FizzBuzz")
+    if i % 3 == 0 or i % 5 == 0 or i % 15 == 0:
+        print("fizz" * (i % 3 == 0) + "buzz" * (i % 5 == 0) + "FizzBuzz" * (i % 15 == 0))
     else:
         print(f"{i}")
